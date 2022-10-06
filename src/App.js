3efab56830a5c90d1a4e4914dca6a932b2ede538
arkadiuszrypulak <arkadiuszrypulak@gmail.com>
2022-10-06 22:59:31 +0200

    More basic dependencies

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
+import { hot } from "react-hot-loader";
 
 const App = () => {
-    return(
-        <div>
-            <h1>Hello it's work! </h1>
-        </div>
-    )
-}
+  return (
+    <div>
+      <h1>Hello it's work! !</h1>
+      <button className="primary-btn">Click</button>
+    </div>
+  );
+};
 
-export default App;
+export default hot(module)(App);
--- webpack.config.js
+++ webpack.config.js
@@ -4,18 +4,34 @@ const webpack = require("webpack");
 module.exports = {
   entry: "./src/index.js",
   mode: "development",
-  output: {
-    filename: "bundle.js",
-    path: path.resolve("dist"),
-    publicPath: "/",
-  },
   module: {
     rules: [
       {
         test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        use: "babel-loader",
+        exclude: /(node_modules)/,
+        loader: "babel-loader",
+        options: { presets: ["@babel/env"] },
+      },
+      {
+        test: /\.less$/,
+        use: ["style-loader", "css-loader", "less-loader"],
       },
     ],
   },
+  resolve: { extensions: ["*", ".js", "jsx"] },
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist/"),
+    publicPath: "/dist/",
+  },
+  devServer: {
+    static: {
+      directory: path.join(__dirname, "public/"),
+    },
+    port: 3000,
+    devMiddleware: {
+      publicPath: "http://localhost:3000/dist/",
+    },
+  },
+  plugins: [new webpack.HotModuleReplacementPlugin()],
 };
